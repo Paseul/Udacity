fed57c56e55b20f30f828ac5726b084ca86a583d
Pasuel <machianb@naver.com>
2019-06-17 15:32:01 +0900

    Navigation_Pixel Update
    Subject: Navigation_Pixel is now learning, but not optimized
    Note: Optimization plan - Update Model
                                           - RGB State to Gray and add more than 3 layers
                                           - enhence reward based memorize static

--- pixel_model.py
+++ pixel_model.py
@@ -2,86 +2,30 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class QNetwork(nn.Module):
-    """Actor (Policy) Model."""
+class DQN(nn.Module):
 
-    def __init__(self, num_input_chnl, action_size, seed, num_filters = [16,32], fc_layers=[64,64]):
-        """Initialize parameters and build model.
-        Params
-        ======
-            num_input_chnl (int): Number of input channels
-            action_size (int): Dimension of each action
-            seed (int): Random seed
-        """
-        super(QNetwork, self).__init__()
-        self.seed = torch.manual_seed(seed)
-       
-        self.conv1 = nn.Conv2d(num_input_chnl, num_filters[0], kernel_size=(3,3), stride=1, padding=(1,1))
-        self.conv1bnorm = nn.BatchNorm2d(num_filters[0])
-        self.conv1relu = nn.ReLU()
-        self.conv1maxp = nn.MaxPool2d(kernel_size=(2,2), stride=(2,2))
-        #self.conv2d_1 = [self.conv1, self.bnorm1, self.relu1, self.maxp1]
+    def __init__(self, state_size, action_size, seed):
+        super(DQN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=5, stride=2)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
+        self.bn3 = nn.BatchNorm2d(32)
 
-        self.conv2 = nn.Conv2d(num_filters[0], num_filters[1], kernel_size=(3,3), stride=1, padding=(1,1))
-        self.conv2bnorm = nn.BatchNorm2d(num_filters[1])
-        self.conv2relu = nn.ReLU()
-        self.conv2maxp = nn.MaxPool2d(kernel_size=(2,2), stride=(2,2))
+        # Number of Linear input connections depends on output of conv2d layers
+        # and therefore the input image size, so compute it.
+        def conv2d_size_out(size, kernel_size = 5, stride = 2):
+            return (size - (kernel_size - 1) - 1) // stride  + 1
+        convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(84)))
+        convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(84)))
+        linear_input_size = convw * convh * 32
+        self.head = nn.Linear(linear_input_size, action_size)
 
-        self.fc1 = nn.Linear(num_filters[1]*21*21, fc_layers[0])
-        self.fc1bnorm = nn.BatchNorm1d(fc_layers[0])
-        self.fc1relu = nn.ReLU()
-
-        self.fc2 = nn.Linear(fc_layers[0], fc_layers[1])
-        self.fc2bnorm = nn.BatchNorm1d(fc_layers[1])
-        self.fc2relu = nn.ReLU()
-        
-        self.fc3 = nn.Linear(fc_layers[1], action_size)
-
-    def forward(self, state):
-        """Build a network that maps state -> action values."""
-        
-        # for conv_1 in self.conv2d_1:
-        #     state = conv_1(state)
-
-        state = self.conv1(state)
-        state = self.conv1bnorm(state)
-        state = self.conv1relu(state)
-        state = self.conv1maxp(state)
-        print(state.shape)
-
-        state = self.conv2(state)
-        state = self.conv2bnorm(state)
-        state = self.conv2relu(state)
-        state = self.conv2maxp(state)
-
-        print(state.shape) #state is of shape Nx32x21x21
-        state = state.reshape((-1,32*21*21)) #reshape the output of conv2 before feeding into fc1 layer
-
-        state = self.fc1(state)
-        state = self.fc1bnorm(state)
-        state = self.fc1relu(state)
-        print(state.shape)
-
-        state = self.fc2(state)
-        state = self.fc2bnorm(state)
-        state = self.fc2relu(state)
-        print(state.shape)
-
-        state = self.fc3(state)
-        print(state.shape)
-
-        return state
-
-'''
-Note: when training, do model_name.train() to properly update batchnorm variables. 
-And during inference, do model_name.eval() to us the batch norm statistics from training time.
-The dqn_agent's act method already handles this.
-
-To speed up inference turn off gradients like this:
-with torch.no_grad():
-    action = model.forward(state)
-
-'''
-
-# If it doesn't work, maybe remove batchnorm.
-
+    # Called with either one element to determine next action, or a batch
+    # during optimization. Returns tensor([[left0exp,right0exp]...]).
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        return self.head(x.view(x.size(0), -1))
