1d44a2746498eaaffc00fd2757ba15db4978eb2e
Paseul <machianb@naver.com>
2019-06-25 22:59:40 +0900

    DDQN Algorithm update
    Applying DDQN Algorithm and training

--- pixel_ddqn/pixel_ddqn_agent.py
+++ pixel_ddqn/pixel_ddqn_agent.py
@@ -104,9 +104,14 @@ class Agent():
             gamma (float): discount factor
         """
         states, actions, rewards, next_states, dones = experiences
-
+        
+        ## (1) Get the best action at next state using orininal Q network
+        best_action_next = self.qnetwork_local(next_states).detach().max(1)[1].unsqueeze(1)
+        ## (2) calculate Q value using target network for next state at these actions, predicted at step 1      
+        Q_targets_next = self.qnetwork_target(next_states).detach().gather(1, best_action_next)
+        
         # Get max predicted Q values (for next states) from target model
-        Q_targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(1)
+        #Q_targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(1)
         # Compute Q targets for current states 
         Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
 
