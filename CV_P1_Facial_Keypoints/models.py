8914e1611d8ca9f39cff416f124a0b29e35afed0
Pasuel <machianb@naver.com>
2020-01-18 10:16:07 +0900

    create CV_P1_Facial_Keypoints

---
+++ CV_P1_Facial_Keypoints/models.py
@@ -0,0 +1,54 @@
+## TODO: define the convolutional neural network architecture
+
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+# can use the below import should you choose to initialize the weights of your Net
+import torch.nn.init as I
+
+
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        
+        ## TODO: Define all the layers of this CNN, the only requirements are:
+        ## 1. This network takes in a square (same width and height), grayscale image as input
+        ## 2. It ends with a linear layer that represents the keypoints
+        ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
+        
+        # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
+        # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
+        self.conv1 = nn.Conv2d(1, 32, 4)
+        self.pool = nn.MaxPool2d(2, 2) 
+        self.conv2 = nn.Conv2d(32, 64, 3)  
+        self.conv3 = nn.Conv2d(64, 128, 2)        
+        self.conv4 = nn.Conv2d(128, 128, 1)
+        self.drop1 = nn.Dropout(p=0.1)
+        self.drop2 = nn.Dropout(p=0.2)
+        self.drop3 = nn.Dropout(p=0.3)
+        self.drop4 = nn.Dropout(p=0.4)
+        self.drop5 = nn.Dropout(p=0.5)
+        self.drop6 = nn.Dropout(p=0.6)
+        self.fc1 = nn.Linear(in_features=21632, out_features=6400)
+        self.fc2 = nn.Linear(in_features=6400, out_features=1000)
+        self.fc3 = nn.Linear(in_features=1000, out_features=136)
+        ## Note that among the layers to add, consider including:
+        # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
+        
+
+        
+    def forward(self, x):
+        ## TODO: Define the feedforward behavior of this model
+        ## x is the input image and, as an example, here you may choose to include a pool/conv step:
+        x = self.drop1(self.pool(F.elu(self.conv1(x))))
+        x = self.drop2(self.pool(F.elu(self.conv2(x))))
+        x = self.drop3(self.pool(F.elu(self.conv3(x))))
+        x = self.drop4(self.pool(F.elu(self.conv4(x))))       
+        x = x.view(x.size(0), -1)
+        x = self.drop5(F.elu(self.fc1(x)))
+        x = self.fc3(self.drop6(F.tanh(self.fc2(x))))
+        # a modified x, having gone through all the layers of your model, should be returned
+        return x
