d56ee30f3dc13ad06f6199d13c8cb4c88fcb2fd4
Paseul <machianb@naver.com>
2020-01-21 16:24:53 +0900

    training successed

--- CV_P2_Image_Captioning/model.py
+++ CV_P2_Image_Captioning/model.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torchvision.models as models
-
+from torch.nn.utils.rnn import pack_padded_sequence
 
 class EncoderCNN(nn.Module):
     def __init__(self, embed_size):
@@ -23,11 +23,29 @@ class EncoderCNN(nn.Module):
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
+        super(DecoderRNN,self).__init__()
+        
+        self.embed = nn.Embedding(vocab_size, embed_size)               
+       
+        self.lstm = nn.LSTM( input_size = embed_size, 
+                             hidden_size = hidden_size, 
+                             num_layers = num_layers, 
+                             batch_first=True
+                           )
+        
+        self.fc = nn.Linear(hidden_size, vocab_size)
     
     def forward(self, features, captions):
-        pass
+        
+        # outputs.shape[1]: 13, captions.shape[1]: 12, (outputs.shape[1]==captions.shape[1]) = False
+        captions = captions[:, :-1]
+        embeddings = self.embed(captions)        
+        inputs = torch.cat((features.unsqueeze(1), embeddings), dim=1)        
+        hiddens, _ = self.lstm(inputs)        
+        outputs = self.fc(hiddens)
+        
+        return outputs
 
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
-        pass
+        return inputs
